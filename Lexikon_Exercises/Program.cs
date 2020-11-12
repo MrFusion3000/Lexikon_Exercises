8e70a47b57622c7bd1d16788f7d29f9db1dab280
MrFusion3000 <nico@crepro.com>
2020-11-12 12:06:34 +0100

    V0.1

--- Lexikon_Exercises/Program.cs
+++ Lexikon_Exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace Lexikon_Exercises
 {
@@ -8,6 +9,7 @@ namespace Lexikon_Exercises
         {
             Program Exercises = new Program();
 
+            Exercises.SplashScreen();
             Exercises.Menu();
             
         }
@@ -15,7 +17,7 @@ namespace Lexikon_Exercises
         public void Menu()
         { // *** Start Main() *** 
           //Huvudmenyn listar alla grundalternativ för programmet
-            Console.SetWindowSize(100, 40);             // Sätt fönsterstorleken till 100 b, 40 h
+            Console.SetWindowSize(100, 40);             // Sätt fönsterstorleken till 100 tecken bredd, 40 tecken höjd
 
             int antalMenyval = 9;                       // initiera antal menyval på huvudmenyn
             int menyVal;                                // initiera heltalsvariabel menyVal för användarinput
@@ -24,18 +26,19 @@ namespace Lexikon_Exercises
             do
             {
                 Console.Clear();                        // Rensa skärmen
-                Console.WriteLine("* ** *** H U V U D M E N Y *** ** *\n");
-                Console.WriteLine("Välj ett alternativ (och tryck Enter):\n");
-                Console.WriteLine("1. Uppg  1 - ");
-                Console.WriteLine("2. Uppg  2 - ");
-                Console.WriteLine("3. Uppg  3 - ");
-                Console.WriteLine("4. Uppg  4 - ");
-                Console.WriteLine("5. Uppg  5 - ");
-                Console.WriteLine("6. Uppg  6 - ");
-                Console.WriteLine("7. Uppg  7 - ");
-                Console.WriteLine("8. Uppg  8 - n");
-                Console.WriteLine("99. Fler Övningar  \n\n");
-                Console.WriteLine("0. Avsluta \n");
+                Console.WriteLine("\n\n\n\t\t\t* ** *** H U V U D M E N Y *** ** *\n");
+                //Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "* ** *** H U V U D M E N Y *** ** *\n"));
+                Console.WriteLine("\t\t\tVälj ett alternativ (och tryck Enter):\n");
+                Console.WriteLine("\t\t\t1. Uppg  1 - Print welcome message  ");
+                Console.WriteLine("\t\t\t2. Uppg  2 - DateTime");
+                Console.WriteLine("\t\t\t3. Uppg  3 - ");
+                Console.WriteLine("\t\t\t4. Uppg  4 - ");
+                Console.WriteLine("\t\t\t5. Uppg  5 - ");
+                Console.WriteLine("\t\t\t6. Uppg  6 - ");
+                Console.WriteLine("\t\t\t7. Uppg  7 - ");
+                Console.WriteLine("\t\t\t8. Uppg  8 - n");
+                Console.WriteLine("\t\t\t99. Fler Övningar  \n\n");
+                Console.WriteLine("\t\t\t0. Avsluta \n");
 
                 menyVal = Check_Valid(antalMenyval);   // Kontroll av godkända tecken och blockering av annat än siffror
 
@@ -44,26 +47,26 @@ namespace Lexikon_Exercises
                     case 1:
                         Exercise1();
                         break;
-                    //case 2:
-                    //    Uppg2();
-                    //    break;
+                    case 2:
+                        Exercise2();
+                        break;
                     //case 3:
-                    //    Uppg3();
+                    //Exercise1();
                     //    break;
                     //case 4:
-                    //    Uppg4();
+                    //Exercise1();
                     //    break;
                     //case 5:
-                    //    Uppg5();
+                    //Exercise1();
                     //    break;
                     //case 6:
-                    //    Uppg6();
+                    //Exercise1();
                     //    break;
                     //case 7:
-                    //    Uppg7();
+                    //Exercise1();
                     //    break;
                     //case 8:
-                    //    Uppg8();
+                    //Exercise1();
                     //    break;
                     //case 99:
                     //    Menu2();
@@ -96,10 +99,33 @@ namespace Lexikon_Exercises
 
         public void SplashScreen()
         {
-            Console.WriteLine(");
+            Console.WriteLine("\n\n\n\n\n");
+            Console.WriteLine("\t\t\tW e l c o m e  t o  t h e");
+            Console.WriteLine("\t\t\tD O S  i n s p i r e d  U n i v e r s e");
+            Console.WriteLine("\t\t\to f");
+            Console.WriteLine("\t\t\tT e s t  E x c e r s i s e s !");
+            Console.WriteLine("\n\n\t\t\tP r e s s  a n y  k e y  t o  c o n t i n u e !");
+            Console.ReadKey();
         }
 
         public void Exercise1()
+        {
+            string firstName = "Nicolas";
+            string lastName = "Lewentorp";
+
+            Console.Clear();                        // Rensa skärmen
+
+            Console.WriteLine("Hello {0} {1}! \nI'm glad to inform you that you are the test subject of my very first assignment!", firstName, lastName);
+
+            Console.ReadKey();
+        }
+
+        public void Exercise2()
+        {
+
+        }
+
+        public void Exercise3()
         {
 
         }
